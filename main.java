75b837230d005c94af46b55afc6ae80cb55f90f1
impulse-py <impulse.you.html@gmail.com>
2019-02-27 13:28:42 -0600

    Update main.java

--- main.java
+++ main.java
@@ -1,13 +1,14 @@
 import java.util.Scanner; // Must import scanner at top of main file
+import packageName.scan;
 
 class Main {
 
-  static lib a = new lib(); // Must instantiate .class file
+  static scan a = new scan(); // Must instantiate .class file
 
   public static void main(String[] args) {
     System.out.println("Please enter a number: ");
     
-    Object x = a.scanf("int");
+    Object x = a.scanf("int"); // In this example, the data type returned will be an integer
     /*
     
      - The variable must be declared as object
