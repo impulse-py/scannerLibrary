626e158fa626f7a0154558c394f6fbce109c25ac
impulse-py <impulse.you.html@gmail.com>
2019-02-27 22:33:07 -0600

    Update and rename lib.java to scan.java

--- scan.java
+++ scan.java
@@ -1,7 +1,38 @@
 import java.util.Scanner;
 
-public class lib {
+public class scan {
   static Scanner key = new Scanner(System.in);
+
+  public static String convStr(Object o) {
+    String s1 = String.valueOf(o);
+
+    return s1;
+  }
+
+  public static int convInt(Object o) {
+    int i = (int)o;
+
+    return i;
+  }
+
+  public static double convDouble(Object o) {
+    double i = (double)o;
+
+    return i;
+  }
+
+  public static char convChar(Object o) {
+    String s1 = o.toString();
+    char c = s1.charAt(0);
+
+    return c;
+  }
+
+  public static boolean convBool(Object o) {
+    boolean b = (Boolean)o;
+    
+    return b;
+  }
   
   public static Object scanf(String dType) {
     Object y;
